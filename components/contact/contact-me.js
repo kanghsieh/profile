5d31afbf51076f2a05e3a3d35c3ed50a0f566267
Kang Hsieh <kanghsieh1990@gmail.com>
2022-05-31 19:13:08 +0200

    added styling to form

--- components/contact/contact-me.js
+++ components/contact/contact-me.js
@@ -7,15 +7,25 @@ const ContactMe = forwardRef((props, ref) => {
       <h1 >Contact Me</h1>
       <div className={styles.form}>
         <form action="" method="post">
-          <label htmlFor="first">First name:</label>
-          <input type="text" id="first" name="first" required />
-          <label htmlFor="last">Last name:</label>
-          <input type="text" id="last" name="last" />
-          <label htmlFor="last">Your email:</label>
-          <input type="text" id="email" name="email" required />
-          <label htmlFor="last">Your message:</label>
-          <textarea row={4} id="message" name="message" required minLength="10" maxLength="1000"/>
-          <button type="submit">Submit</button>
+          <div className={styles.formGroup}>
+            <label htmlFor="first">First name:</label>
+            <input type="text" id="first" name="first" required />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="last">Last name:</label>
+            <input type="text" id="last" name="last" />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="last">Your email:</label>
+            <input type="text" id="email" name="email" required />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="last">Your message:</label>
+            <textarea row={4} id="message" name="message" required minLength="10" maxLength="1000"/>
+          </div>
+          <div className={styles.formGroup}>
+            <button type="submit">Submit</button>
+          </div>
         </form>
       </div>
     </div>
